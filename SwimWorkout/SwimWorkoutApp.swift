7ef78d888ddb6e216844e89612427f34f181c3a9
Petr Kravnik <petr.kravnik@gmail.com>
2024-11-17 19:49:44 +0100

    Initial Commit

---
+++ SwimWorkout/SwimWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimWorkoutApp.swift
+//  SwimWorkout
+//
+//  Created by Petr Kravnik on 17.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwimWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
